5eb94c5665a12f0aeaee87bec84d0aeec4717f2d
Ivan Gousev <vanyagousev@gmail.com>
2023-09-16 16:50:13 +0300

    sycn

--- 16.09.23_semi/BS_s171_n20.cpp
+++ 16.09.23_semi/BS_s171_n20.cpp
@@ -11,9 +11,10 @@ void fill_vectors(std::vector<std::string>& names, std::vector<int>& scores,std:
         scores.push_back(numbuff);
         names.push_back(namebuff);
         is_deleated.push_back(false);
-        std::cin >>namebuff>>numbuff;       
+        std::cin >>namebuff>>numbuff;
     }
 }
+
 
 void find_and_del_doubles(std::vector<std::string>& names, std::vector<int>& scores, std::vector<bool>& is_deleated){
     std::cout<<"\n\n";
@@ -27,6 +28,7 @@ void find_and_del_doubles(std::vector<std::string>& names, std::vector<int>& sco
     }
 }
 
+
 void print_all(std::vector<std::string>& names, std::vector<int>& scores, std::vector<bool>& is_deleated){
     for(size_t i=0;i<names.size();i++){
         if(!is_deleated[i]){
@@ -35,6 +37,7 @@ void print_all(std::vector<std::string>& names, std::vector<int>& scores, std::v
     }
 }
 
+
 int find_by_name(std::string namebuff,std::vector<std::string>& names, std::vector<int>& scores, std::vector<bool>& is_deleated){
     for(size_t i = 0; i<scores.size();i++){
         if(names[i] == namebuff && !is_deleated[i]){
@@ -42,16 +45,20 @@ int find_by_name(std::string namebuff,std::vector<std::string>& names, std::vect
         }
     }
     return -1;
+}
 
-int find_by_score(int num,std::vector<std::string>& names, std::vector<int>& scores, std::vector<bool>& is_deleated){
-    flag = true;
+
+std::vector<int> find_by_score(int num,std::vector<std::string>& names, std::vector<int>& scores, std::vector<bool>& is_deleated){
+    std::vector<int> ret;
     for(size_t i = 0; i<scores.size();i++){
         if(scores[i] == num && !is_deleated[i]){
-            return i;
+            ret.push_back(i);
         }
     }
-    return -1;
+    return ret;
 }
+
+
 
 int main(){
     std::vector<int> scores;
@@ -61,7 +68,10 @@ int main(){
     bool flag;
     std::string namebuff = "";
     int numbuff=0;
-
+    char chbuff;
+    std::cin >> numbuff>> namebuff;
+    std::cout << numbuff <<" "<< namebuff;
+    return 0;
     fill_vectors(names,scores,is_deleated);
 
     find_and_del_doubles(names,scores, is_deleated);
@@ -70,7 +80,7 @@ int main(){
     print_all(names,scores, is_deleated);
 
 
-
+    std::cout<<"\n\nпоиск по имени \n";
     std::cin>>namebuff;
     while (namebuff != "NoName"){
         int i = find_by_name(namebuff,names,scores, is_deleated);
@@ -83,21 +93,55 @@ int main(){
         std::cin>>namebuff;
     }
 
-
+    std::cout<<"\n\nпоиск по баллу \n";
     std::cin>>numbuff;
     while (numbuff >-1){
-        flag = true;
-        for(size_t i = 0; i<scores.size();i++){
-            if(scores[i] == numbuff){
-                std::cout<<" "<<names[i]<<std::endl;
-                flag = false;
-            }
-        }
-        if(flag){
+        std::vector<int> i = find_by_score(numbuff,names,scores, is_deleated);
+        if(i.size() == 0){
             std::cout<<"Балл не найден\n";
+        }
+        for(auto j:i){
+            std::cout <<"\t"<<names[j]<<std::endl;
         }
         std::cin>>numbuff;
     }
+
+    std::cout<<"\n\nрежим изменений, введите \n<имя> [+/-/*/=] <число> \n";
+    std::cin>>namebuff>>chbuff>>numbuff;
+    while (namebuff != "NoName"){
+        flag = true;
+        int i = find_by_name(namebuff,names,scores, is_deleated);
+        if(i==-1){
+            std::cout<<"Имя не найдено\n";
+        }
+        else{
+            switch (chbuff)  {
+                case '+':
+                    scores[i]+=numbuff;
+                    break;
+                case '-':
+                    scores[i]-=numbuff;
+                    break;
+                case '*':
+                    scores[i]*=numbuff;
+                    break;
+                case '=':
+                    scores[i]=numbuff;
+                    break;
+                default:
+                    std::cout<<"действие неверно\n";
+                    flag = false;
+                    break;
+}
+        if(flag){
+            std::cout<<namebuff<<" score is now "<<scores[i]<<std::endl;
+        }
+        }
+        std::cin>>namebuff>>chbuff>>numbuff;
+    }
+    std::cout<<"\n\n";
+    print_all(names,scores, is_deleated);
+
 
     return 0;
 }
